ddb010c1ed02887d611fc16a9f737111aa1bbf69
monalisanpereira <monalisanpereira@gmail.com>
2024-12-08 23:59:00 +0900

    complete day 6 part 2

--- day6.ts
+++ day6.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import 'dotenv/config';
+import { isEqual } from 'lodash';
 
 const testInput = `....#.....
 .........#
@@ -13,6 +14,10 @@ const testInput = `....#.....
 ......#...
 `
 
+// variables
+
+const guard = "^";
+const obstruction = "#";
 const directions = [
     "up",
     "right",
@@ -27,9 +32,17 @@ const nextDirectionMap = {
     left: "up" as Direction,
 };
 
+// types
+
 type Direction = typeof directions[number];
 type Map = string[];
 type Position = [number, number];
+type PositionWithDirection = {
+    position: Position;
+    direction: Direction;
+};
+
+// help functions
 
 const parseMap = (data: string): Map => {
     return data.split("\n").slice(0, -1);
@@ -37,8 +50,8 @@ const parseMap = (data: string): Map => {
 
 const getGuardPosition = (map: Map): Position => {
     for (const stripe of map) {
-        if (stripe.includes("^"))
-            return [map.indexOf(stripe), stripe.indexOf("^")];
+        if (stripe.includes(guard))
+            return [map.indexOf(stripe), stripe.indexOf(guard)];
     }
 }
 
@@ -47,27 +60,42 @@ const isValidPosition = (position: Position, map: Map): boolean => {
         position.some(v => v < 0) ||
         position.some(v => v >= map[0].length)
     ) return false;
+
     return true;
 }
 
 const includesPosition = (positions: Position[], position: Position): boolean => {
     for (const referencePosition of positions) {
-        if (position.length === referencePosition.length &&
-            position.every((value, index) => value === referencePosition[index])
-        ) return true;
+        if (isEqual(position, referencePosition)) return true;
     }
 
     return false;
 }
 
-const move = (map: Map, startingPosition: Position, direction: Direction, distinctPositions: Position[]): [Position, Direction] => {
+const includesPositionWithDirection = (
+    positionsWithDirections: PositionWithDirection[],
+    positionWithDirection: PositionWithDirection
+): boolean => {
+    for (const referencePositionWithDirection of positionsWithDirections) {
+        if (isEqual(positionWithDirection, referencePositionWithDirection)) return true;
+    }
+
+    return false;
+}
+
+const move = (
+    map: Map, 
+    startingPosition: Position,
+    direction: Direction,
+    distinctPositions: Position[]
+): PositionWithDirection => {
     let currentPosition = startingPosition;
     const nextDirection = nextDirectionMap[direction];
 
     while (true) {
         let nextPosition: Position;
         const [xc, yc] = currentPosition;
-        
+
         switch (direction) {
             case "up":
                 nextPosition = [xc - 1, yc];
@@ -85,11 +113,9 @@ const move = (map: Map, startingPosition: Position, direction: Direction, distin
 
         const [xn, yn] = nextPosition;
 
-        if (!isValidPosition(nextPosition, map)) return [nextPosition, nextDirection];
+        if (!isValidPosition(nextPosition, map)) return { position: nextPosition, direction: nextDirection};
 
-        // console.log(map[xn][yn]);
-
-        if (map[xn][yn] === "#") return [currentPosition, nextDirection];
+        if (map[xn][yn] === obstruction) return { position: currentPosition, direction: nextDirection};
 
         if (!includesPosition(distinctPositions, nextPosition)) {
             distinctPositions.push(nextPosition);
@@ -100,23 +126,66 @@ const move = (map: Map, startingPosition: Position, direction: Direction, distin
 
 }
 
-const countDistinctPositions = (map: Map): number => {
-    const distinctPositions: Position[] = [];
-    const initialPosition = getGuardPosition(map);
-    
-    distinctPositions.push(initialPosition);
-    // console.log(distinctPositions);
+const placeObstruction = (map: Map, position: Position): Map => {
+    const [x, y] = position;
 
-    let currentPosition = initialPosition;
-    let currentDirection: Direction = "up";
-
-    while (isValidPosition(currentPosition, map)) {
-        [currentPosition, currentDirection] = move(map, currentPosition, currentDirection, distinctPositions);
-        // console.log(distinctPositions);
-        // console.log(distinctPositions.length);
+    if (map[x][y] === guard) {
+        throw Error("Impossible to place obstruction where the guard is!");
     }
 
-    return distinctPositions.length;
+    const sliceOfMap = map[x];
+    const sliceOfMapWithObstruction = sliceOfMap.substring(0, y) + obstruction + sliceOfMap.substring(y + 1);
+    const mapWithObstruction = map.slice(0, x).concat(sliceOfMapWithObstruction, map.slice(x + 1));
+
+    return mapWithObstruction;
+}
+
+const checkIfMapLeadsToLoop = (map: Map): boolean => {
+    const guardsPositions: Position[] = [];
+    const cornerPositions: PositionWithDirection[] = [];
+    const initialPosition = getGuardPosition(map);
+    
+    guardsPositions.push(initialPosition);
+
+    let currentPositionWithDirection: PositionWithDirection = { position: initialPosition, direction: "up" };
+
+    while (isValidPosition(currentPositionWithDirection.position, map)) {
+        currentPositionWithDirection = move(map, currentPositionWithDirection.position, currentPositionWithDirection.direction, guardsPositions);
+        if (includesPositionWithDirection(cornerPositions, currentPositionWithDirection)) return true;
+        cornerPositions.push(currentPositionWithDirection);
+    }
+    
+    return false;
+}
+
+// main functions
+
+const getAllDistinctGuardsPositions = (map: Map): Position[] => {
+    const distinctGuardsPositions: Position[] = [];
+    const initialPosition = getGuardPosition(map);
+    
+    distinctGuardsPositions.push(initialPosition);
+
+    let currentPositionWithDirection: PositionWithDirection = { position: initialPosition, direction: "up" };
+
+    while (isValidPosition(currentPositionWithDirection.position, map)) {
+        currentPositionWithDirection = move(map, currentPositionWithDirection.position, currentPositionWithDirection.direction, distinctGuardsPositions);
+    }
+
+    return distinctGuardsPositions;
+}
+
+const getAllObstructionPositionsLeadingToLoop = (map: Map, possibleObstructions: Position[]): Position[] => {
+    const obstructionPositionsLeadingToLoop: Position[] = [];
+    
+    for (const position of possibleObstructions) {
+        console.log(`Not in a loop, now checking ${position}...`);
+        const mapWithObstruction = placeObstruction(map, position);
+        if (checkIfMapLeadsToLoop(mapWithObstruction))
+            obstructionPositionsLeadingToLoop.push(position);
+    }
+
+    return obstructionPositionsLeadingToLoop;
 }
 
 axios({
@@ -132,6 +201,10 @@ axios({
     .then((response) => {
         // const map = parseMap(testInput);
         const map = parseMap(response.data);
-        const distinctPositions = countDistinctPositions(map);
-        console.log(`The total number of distinct positions is: ${distinctPositions}`);
+
+        const distinctGuardsPositions = getAllDistinctGuardsPositions(map);
+        const obstructionPositionsLeadingToLoop = getAllObstructionPositionsLeadingToLoop(map, distinctGuardsPositions.slice(1));
+
+        console.log(`The total number of the guard's distinct positions is: ${distinctGuardsPositions.length}`);
+        console.log(`The total number of obstruction positions leading to a loop is: ${obstructionPositionsLeadingToLoop.length}`);
     }); 
